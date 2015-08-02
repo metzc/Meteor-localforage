c01898e7aeb21f988c55634053372d45d62191ed
Morten Henriksen <mh@gi2.dk>
2015-08-02 21:56:50 +0200

    Initial commit

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-Meteor-localforage
+raix:localforage
 ==================
 
-Offline storage, improved. Wraps IndexedDB, WebSQL, or localStorage using a simple but powerful API.
+Offline storage, improved. Wraps IndexedDB, WebSQL, or localStorage using a simple but powerful API. On cordova we also support SQLite.
 
 [Mozilla's LocalForage](https://github.com/mozilla/localForage)
+
+#### Note
+This package exports the `LocalForage` class and the default `localForage` instance:
+
+```js
+  localForage = new LocalForage();
+```
+
+Also note that SQLite is added to the list when using `LocalForage` on cordova applications.
--- package.js
+++ package.js
@@ -1,13 +1,23 @@
 Package.describe({
-  version: '0.0.0',
-  summary: "Offline storage, improved. Wraps IndexedDB, WebSQL, or localStorage using a simple but powerful API."
+  name: 'raix:localforage',
+  version: '1.2.4',
+  summary: "Offline storage, improved. Wraps SQLite, IndexedDB, WebSQL, or localStorage using a simple but powerful API."
 });
 
 Package.on_use(function (api) {
-  api.add_files('drivers/indexeddb.js', 'client');
-  api.add_files('drivers/localstorage.js', 'client');
-  api.add_files('drivers/websql.js', 'client');
-  api.add_files('localforage.js', 'client');
+  // api.versionsFrom('1.2');
+  api.use('ecmascript', 'client');
 
-  //api.export('$', 'client');
+  api.use('raix:localforage-localstorage@1.2.4', 'client');
+  api.use('raix:localforage-websql@1.2.4', 'client');
+  api.use('raix:localforage-indexeddb@1.2.4', 'client');
+  // Specially added driver for cordova
+  api.use('raix:localforage-sqlite@1.0.0', 'web.cordova');
+
+  // LocalForage
+  api.addFiles('localforage.js', 'client');
+  api.export('localForage', 'client');
+
+  // Also export the LocalForage class
+  api.export('LocalForage', 'client');
 });
